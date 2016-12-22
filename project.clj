bdf2a30154f6ebb7ff1bee5754187987505483c9
ilevd <ilevd@mail.ru>
2016-12-22 16:00:15 +0700

    Add .travis

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:aot playground.core
+  :uberjar-name "pg-standalone.jar"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
 
