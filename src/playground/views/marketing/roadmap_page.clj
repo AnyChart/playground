f4bf27de94392d7875d5cd933a8766943f274b11
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-05-16 22:45:24 +0800

    Cleanup roadmap

--- src/playground/views/marketing/roadmap_page.clj
+++ src/playground/views/marketing/roadmap_page.clj
@@ -26,53 +26,48 @@
             [:li "все маркетинговые страницы которые не готовы к релизу"]]]
           [:li "При помощи автоматической утилиты ENV-544, переделываем все примеры на новый формат"]
           [:li "Проставляем теги, как минимум для галереи, как максимум еще и для доков"]
-          [:li "Имплементим дизайн"]
-          [:li "Вещи из старого PG которых не будет в первом релизе"
-           [:ul
-            [:li "Фул скрин"]
-            [:li "Поиск"]
-            [:li "Сохранение в картинку через UI плейграунда"]
-            ]]
-          [:li "Вещи из старого PG которые планируется дропнуть " [:b "совсем"]
-           [:ul
-            [:li "Интеграция с jsfiddle, теперь незачем"]
-            [:li "Кнопка смены темы, это должно делаться через Quick Add и код, но не через кнопку"]
-            [:li "Tag manager, я решительно против дефолтного конфига тег менеджера в этом проекте:
-            Убивает UX, Тормозит загрузку страницы, за плагин Holdon Stranger в сервисе вообще отдельный котел в аду нужен"]
-            ]]
-          ]
-         ]
+          [:li "Имплементим дизайн"]]]
         [:div.row
          [:h3 "Version 1.0.0 - Stable release"]
          [:ul
           [:li "Фиксим баги если они есть"]
-          [:li "Берем один - два итема из Features list"]
-         ]]
+          [:li "Берем один - два итема из Features list"]]]
         [:div.row
          [:h3 "Version 1.1.0 - TBA"]
          [:ul
-          [:li "Набираем из Features list"]
-          ]]
+          [:li "Набираем из Features list"]]]
+        [:div.row
+         [:h3 "Drop list"]
+         [:p "Вещи которые есть на старом PG и их планируется дропнуть"]
+         [:div.col-md-3
+          [:h5 "Tag Manager (текущий конфиг)"]
+          [:ul
+           [:li "Кнопка LiveChart'a не уместна"]
+           [:li "Holdon Stranger тут не уместен"]
+           [:li "Тормозит загрузку страницы"]]]
+         [:div.col-md-3
+          [:h5 "Chart related buttons"]
+          [:ul
+           [:li "PNG/PDF/JPG/SVG"]
+           [:li "CSV/XLSX"]
+           [:li "Вкладка JSON"]
+           [:li "Смена темы"]]]
+         [:div.col-md-3
+          [:h5 "Переработано/не актуально"]
+          [:ul
+           [:li "Список примеров и категорий"]
+           [:li "Интеграция с jsfiddle"]]]]
         [:div.row
          [:h3 "Features list"]
          [:div.col-md-3
           [:h5 "Export and Embeding"]
           [:ul
            [:li "Copy btn for HTML/CSS/JS/Result windows"]
+           [:li "Full source as text/gist"]
            [:li "Download button"]
            [:li "Embed as iframe link"]
            [:li "Embed as Script with code"]
-           [:li "Embed as Script with link"]
-           ]]
-         [:div.col-md-3
-          [:h5 "Export to"]
-          [:ul
-           [:li "PNG"]
-           [:li "PDF"]
-           [:li "JPG"]
-           [:li "SVG"]
-           [:li "CSV"]
-           [:li "XLSX"]]]
+           [:li "Embed as Script with link"]]]
          [:div.col-md-3
           [:h5 "Share to"]
           [:ul
@@ -82,9 +77,25 @@
            [:li "Pinterest"]
            [:li "Instagram"]
            [:li "XLSX"]]]
-         [:div.col-md-3 "Full screen"]
-         [:div.col-md-3 "Print"]
-         ]
+         [:div.col-md-3
+          [:h5 "Search"]
+          [:ul
+           [:li "По примерам (имена/описания)"]
+           [:li "По тегам"]
+           [:li "По датасетам"]
+           [:li "По коду"]
+           [:li "С фильтрами по проекту/тегу/дата сету"]]]
+         [:div.col-md-3
+          [:h5 "Разное"]
+          [:ul
+           [:li "Full screen"]
+           [:li "Print"]]]
+         [:div.col-md-3
+          [:h5 "Preprocessors"]
+          [:ul
+           [:li "JS -CofeScript, TypesScript, CljoreScript, more?"]
+           [:li "CSS - LESS, SASS, more?"]
+           [:li "Markup - Markdown, more?\""]]]]
         ]]
 
       (page/footer (:repos data) (:tags data))]]))
