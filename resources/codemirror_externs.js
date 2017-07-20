556e06334c3ffb59d793dacf302eb9810ab3f577
ilevd <ilevd@mail.ru>
2017-07-20 16:02:39 +0700

    Add CodeMirror externs

--- resources/codemirror_externs.js
+++ resources/codemirror_externs.js
@@ -1,5 +1,7 @@
 var CodeMirror = function() {}
 CodeMirror.on = function(event, func){}
 CodeMirror.getValue = function(){}
+CodeMirror.getDoc = function(){}
+CodeMirror.getDoc.prototype.setValue = function(code){}
 
 var Clipboard = function(elem, settings) {}
