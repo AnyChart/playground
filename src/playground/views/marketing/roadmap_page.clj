feda9118390cbe6a1f50e7a83a350564856a617b
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-10-09 16:28:29 +0800

    Update roadmap

--- src/playground/views/marketing/roadmap_page.clj
+++ src/playground/views/marketing/roadmap_page.clj
@@ -15,10 +15,11 @@
        [:div.container-fluid.content-container
 
         [:h1.page-caption "Roadmap"]
+        [:p "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut culpa deserunt id numquam sed temporibus voluptatem! Animi delectus dolore eos excepturi optio, quasi quibusdam reiciendis suscipit tenetur ullam velit."]
 
         [:div.line-container
          [:div
-          [:h2 "Version 1.1.0" [:span.muted "Likes, Download and Share"]]
+          [:h2 "Version 1.1.0" [:span.muted "Like, Download and Share"]]
           [:div
            [:ul
             [:li "Ability to download samples as HTML file, PDF file or Vector/Raster image"]
@@ -26,10 +27,27 @@
             [:li "Ability to like samples"]]]]
 
          [:div
-          [:h2 "Version 1.2.0" [:span.muted "Search Everywhare"]]
+          [:h2 "Version 1.2.0" [:span.muted "Search for Samples"]]
           [:div
            [:ul
-            [:li "Ability to search samples by names, tags and used data sets"]]]]]]]
+            [:li "Ability to search samples by names, description, tags and used data sets"]]]]
+
+         [:div
+          [:h2 "Version 1.3.0" [:span.muted "Documentation and API Reference"]]
+          [:div
+           [:ul
+            [:li "Плейграунд будет по коду примера саджестить статьи, ссылки на API Reference и чартопедию на странице примера и в редакторе."]]]]
+
+         [:div
+          [:h2 "Version 2.0.0" [:span.muted "Personalization"]]
+          [:div
+           [:ul
+            [:li "Личный кабинет"]
+            [:li "Privacy Settings"]
+            [:li "Коллекции примеров"]
+            [:li "Assets Hosting"]
+            ]]]
+         ]]]
 
       (page/footer (:repos data) (:tags data) (:data-sets data))]
      [:script {:src "/jquery/jquery.min.js"}]
