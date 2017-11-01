e7d73b9fe0ba57156d25b65affe94a2d8eabee06
ilevd <ilevd@mail.ru>
2017-11-01 13:10:50 +0700

    Adjust tag manager

--- src/playground/views/editor/editor_page.clj
+++ src/playground/views/editor/editor_page.clj
@@ -89,9 +89,13 @@
      [:link {:href "/css/editor.css" :rel "stylesheet"}]
      [:link {:href "https://cdn.anychart.com/fonts/2.7.2/anychart.css" :rel "stylesheet"}]
      [:link {:href "https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&subset=greek" :type "text/css" :rel "stylesheet"}]
+     [:script {:type "text/javascript"} "window.HIDE_SHARING_BUTTONS = true;"]
      page/head-tag-manager]
 
-    [:body page/body-tag-manager
+
+    [:body
+     [:script {:type "text/javascript"} "window.HIDE_SHARING_BUTTONS = true;"]
+     page/body-tag-manager
 
      [:div {:style "display: none;"}
       [:textarea#transit-data data]]
