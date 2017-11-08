414e618d7411ae08a7fa7db943191832aa3d0158
ilevd <ilevd@mail.ru>
2017-11-08 16:48:05 +0700

    Site js: fix function name

--- src-site-cljs/playground/site/landing.cljs
+++ src-site-cljs/playground/site/landing.cljs
@@ -24,7 +24,7 @@
   (set! (.-title js/document) title))
 
 
-(defn str->elem [s]
+(defn str-to-elem [s]
   (let [d (.createElement js/document "div")]
     (set! (.-innerHTML d) s)
     (.-firstChild d)))
@@ -54,7 +54,7 @@
   (if (pos? @*page)
     (when-not (.getElementById js/document id)
       (let [container (.getElementById js/document "prev-next-buttons")]
-        (.prepend container (str->elem (h/html (prev-next-buttons-common/prev-button id @*page url))))
+        (.prepend container (str-to-elem (h/html (prev-next-buttons-common/prev-button id @*page url))))
         (add-prev-event id *page load-fn)))
     (if-let [btn (.getElementById js/document id)]
       (.remove btn))))
@@ -64,7 +64,7 @@
   (if-not end
     (when-not (.getElementById js/document id)
       (let [container (.getElementById js/document "prev-next-buttons")]
-        (.append container (str->elem (h/html (prev-next-buttons-common/next-button id @*page url))))
+        (.append container (str-to-elem (h/html (prev-next-buttons-common/next-button id @*page url))))
         (add-next-event id *page load-fn)))
     (if-let [btn (.getElementById js/document id)]
       (.remove btn))))
