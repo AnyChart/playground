d1ea51570a69972228035e518a364068288a098e
ilevd <ilevd@mail.ru>
2018-05-31 16:40:19 +0700

    Preview generator improvement

--- src/playground/preview_generator/phantom_embed.clj
+++ src/playground/preview_generator/phantom_embed.clj
@@ -56,139 +56,154 @@
   (.add drivers-queue driver))
 
 
-(defn exec-script-to-png [d sample image-path]
+(defn sample-to-png [d sample image-path]
   ;(timbre/info "Generate image -" (:url sample) (:image-url sample) (keys sample))
-  (let [prev-handles (.getWindowHandles (:webdriver d))]
-    (execute-script d "window.open(\"\")")
-    (let [new-handles (.getWindowHandles (:webdriver d))
-          new-handle (first (clojure.set/difference (set new-handles) (set prev-handles)))
-          prev-handle (first prev-handles)]
-      (.window (.switchTo (:webdriver d)) new-handle)
-      (.setSize (.window (.manage (:webdriver d))) (Dimension. phantom-viewport-width
-                                                               phantom-viewport-height))
-      ;(prn "Window handles on start: " prev-handles)
-      ;(prn "Current: " (.getWindowHandle (:webdriver d)))
-      (let [startup
-            (try
-              (execute-script d "document.body.style.margin = 0;
+  (try
+    (let [prev-handles (.getWindowHandles (:webdriver d))]
+     (execute-script d "window.open(\"\")")
+     (let [new-handles (.getWindowHandles (:webdriver d))
+           new-handle (first (clojure.set/difference (set new-handles) (set prev-handles)))
+           prev-handle (first prev-handles)]
+       (.window (.switchTo (:webdriver d)) new-handle)
+       (.setSize (.window (.manage (:webdriver d))) (Dimension. phantom-viewport-width
+                                                                phantom-viewport-height))
+       ;(prn "Window handles on start: " prev-handles)
+       ;(prn "Current: " (.getWindowHandle (:webdriver d)))
+       (let [startup
+             (try
+               (execute-script d "document.body.style.margin = 0;
                                  document.body.innerHTML = arguments[0]"
-                              [(:markup sample)])
-              (catch Exception e (str "Failed to execute startup script\n" (.getMessage e))))
+                               [(:markup sample)])
+               (catch Exception e (str "Failed to execute startup script\n" (.getMessage e))))
 
-            styles
-            (try
-              (doseq [style (:styles-data sample)]
-                (execute-script d "var css = document.createElement('style');
+             styles
+             (try
+               (doseq [style (:styles-data sample)]
+                 (execute-script d "var css = document.createElement('style');
                                    css.type = 'text/css';
                                    css.innerHTML = arguments[0];
                                    document.body.appendChild(css);"
-                                [style]))
-              (catch Exception e (str "Failed to set css\n" (.getMessage e))))
+                                 [style]))
+               (catch Exception e (str "Failed to set css\n" (.getMessage e))))
 
-            styles-urls
-            (try
-              (doseq [style-url (download/get-urls (:styles sample))]
-                (execute-script d "var link = document.createElement('link');
+             styles-urls
+             (try
+               (doseq [style-url (download/get-urls (:styles sample))]
+                 (execute-script d "var link = document.createElement('link');
                                    link.type = 'text/css';
                                    link.rel = 'stylesheet';
                                    link.href = arguments[0];
                                    document.head.appendChild(link);"
-                                [style-url]))
-              (catch Exception e (str "Failed to set css\n" (.getMessage e))))
+                                 [style-url]))
+               (catch Exception e (str "Failed to set css\n" (.getMessage e))))
 
-            css
-            (try
-              (execute-script d "var css = document.createElement('style');
+             css
+             (try
+               (execute-script d "var css = document.createElement('style');
                                  css.type = 'text/css';
                                  css.innerHTML = arguments[0];
                                  document.body.appendChild(css);"
-                              [(:style sample)])
-              (catch Exception e (str "Failed to set css\n" (.getMessage e))))
+                               [(:style sample)])
+               (catch Exception e (str "Failed to set css\n" (.getMessage e))))
 
-            scripts
-            (try
-              (doseq [script (download/get-urls (:scripts sample))]
-                (execute-script d "var s=window.document.createElement('script')
+             scripts
+             (try
+               (doseq [script (download/get-urls (:scripts sample))]
+                 (execute-script d "var s=window.document.createElement('script')
                                    s.innerHTML = arguments[0];
                                    window.document.head.appendChild(s);" [script]))
-              (catch Exception e (str "Failed to execute external sample script\n" (.getMessage e))))
+               (catch Exception e (str "Failed to execute external sample script\n" (.getMessage e))))
 
-            script
-            (try
-              (execute-script d "var s=window.document.createElement('script')
+             script
+             (try
+               (execute-script d "var s=window.document.createElement('script')
                                  s.innerHTML = arguments[0];
                                  window.document.head.appendChild(s);" [(:code sample)])
-              (catch Exception e (str "Failed to execute Script\n" (.getMessage e))))
+               (catch Exception e (str "Failed to execute Script\n" (.getMessage e))))
 
-            triggerload
-            (execute-script d "var evt = window.document.createEvent('Event');
+             triggerload
+             (execute-script d "var evt = window.document.createEvent('Event');
                                evt.initEvent('load', false, false);
                                window.dispatchEvent(evt);")
 
-            waiting
-            (try
-              (let [now (System/currentTimeMillis)]
-                (loop []
-                  (if (not-empty (elements d "svg"))
-                    (do (Thread/sleep 200) nil)
-                    (if (> (System/currentTimeMillis) (+ now 10000))
-                      (do (Thread/sleep 200) nil)
-                      (do (Thread/sleep 20) (recur))))))
-              (catch Exception e (str "Failed to wait for SVG\n" (.getMessage e))))
+             waiting
+             (try
+               (let [now (System/currentTimeMillis)]
+                 (loop []
+                   (if (not-empty (elements d "svg"))
+                     (do (Thread/sleep 200) nil)
+                     (if (> (System/currentTimeMillis) (+ now 10000))
+                       (do (Thread/sleep 200) nil)
+                       (do (Thread/sleep 20) (recur))))))
+               (catch Exception e (str "Failed to wait for SVG\n" (.getMessage e))))
 
-            screenshot (take-screenshot d :bytes nil)
+             screenshot (take-screenshot d :bytes nil)
 
-            shutdown
-            (try
-              (execute-script d "while (document.body.hasChildNodes()){document.body.removeChild(document.body.lastChild);}", [])
-              (catch Exception e (str "Failed to execute Shoutdown Script\n" (.getMessage e))))
+             shutdown
+             (try
+               (execute-script d "while (document.body.hasChildNodes()){document.body.removeChild(document.body.lastChild);}", [])
+               (catch Exception e (str "Failed to execute Shoutdown Script\n" (.getMessage e))))
 
-            results [startup
-                     styles
-                     styles-urls
-                     css
-                     scripts
-                     script
-                     waiting
-                     triggerload
-                     shutdown]
+             results [startup
+                      styles
+                      styles-urls
+                      css
+                      scripts
+                      script
+                      waiting
+                      triggerload
+                      shutdown]
 
-            error (some identity results)
+             error (some identity results)
 
-            scaled-image (Scalr/resize (ImageIO/read (ByteArrayInputStream. screenshot))
-                                       Scalr$Method/ULTRA_QUALITY
-                                       Scalr$Mode/FIT_TO_WIDTH
-                                       image-width
-                                       image-height
-                                       (into-array BufferedImageOp [Scalr/OP_ANTIALIAS]))]
+             scaled-image (Scalr/resize (ImageIO/read (ByteArrayInputStream. screenshot))
+                                        Scalr$Method/ULTRA_QUALITY
+                                        Scalr$Mode/FIT_TO_WIDTH
+                                        image-width
+                                        image-height
+                                        (into-array BufferedImageOp [Scalr/OP_ANTIALIAS]))]
 
-        (execute-script d "window.close(\"\")")
-        (.window (.switchTo (:webdriver d)) prev-handle)
-        ;(prn "Window handles on end: " (.getWindowHandles (:webdriver d)))
+         (execute-script d "window.close(\"\")")
+         (.window (.switchTo (:webdriver d)) prev-handle)
+         ;(prn "Window handles on end: " (.getWindowHandles (:webdriver d)))
 
-        ;(with-open [out (output-stream (clojure.java.io/file image-path))]
-        ;  (.write out (screenshot)))
+         ;(with-open [out (output-stream (clojure.java.io/file image-path))]
+         ;  (.write out (screenshot)))
 
-        (ImageIO/write scaled-image "png" (io/file image-path))
-        (shell/sh "pngquant" "--force" "--ext" ".png" image-path)
+         (ImageIO/write scaled-image "png" (io/file image-path))
+         (shell/sh "pngquant" "--force" "--ext" ".png" image-path)
 
-        (when error (timbre/info "ERROR:" (:image-url sample) (pr-str (filter some? results))))
+         (when error (timbre/info "ERROR:" (:image-url sample) (pr-str (filter some? results))))
 
-        ;(prn "Generate preview: " (:id sample) " " (:url sample) " " error)
-        (if error
-          {:error error :id (:id sample) :url (:url sample)}
-          (select-keys sample [:id :url :full-url]))))))
+         ;(prn "Generate preview: " (:id sample) " " (:url sample) " " error)
+         (if error
+           {:error error :id (:id sample) :url (:url sample)}
+           (select-keys sample [:id :url :full-url])))))
+
+    (catch Exception e
+      (timbre/error "Sample to png error: " e)
+      {:error (str "Sample to png error occured: " e)
+       :id (:id sample) :url (:url sample)})))
 
 
 (defn generate-image [sample generator]
   (try
+
     (if-let [driver (get-free-driver (-> generator :drivers-queue))]
       (let [images-dir (-> generator :conf :images-dir)
             image-path (utils/image-path images-dir sample)
-            res (exec-script-to-png driver sample image-path)]
-        (return-driver driver (-> generator :drivers-queue))
+            res (sample-to-png driver sample image-path)]
+
+        (if (:error res)
+          (do
+            (try (.quit driver)
+                 (catch Exception e (timbre/error "Quit driver error: " e)))
+            (return-driver (create-driver) (-> generator :drivers-queue)))
+          (return-driver driver (-> generator :drivers-queue)))
+
         res)
       {:error "Driver isn't available\n" :id (:id sample) :url (:url sample)})
+
     (catch Exception e
       (do
         (timbre/info "generation failed for" (:url sample) " exception:" e)
