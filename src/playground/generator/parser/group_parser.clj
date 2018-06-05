ff9125804025e2fa8157bc83e9d3da8c205b315b
ilevd <ilevd@mail.ru>
2018-06-05 15:54:13 +0700

    Sample parser: src param support

--- src/playground/generator/parser/group_parser.clj
+++ src/playground/generator/parser/group_parser.clj
@@ -4,13 +4,18 @@
             [playground.generator.parser.sample-parser :as sample-parser]
             [clojure.string :as string]))
 
+
 (defn inner-path [base-path sample-path]
   (let [in-path-with-name (subs sample-path (count base-path) (count sample-path))
         in-path (string/join "/" (butlast (string/split in-path-with-name #"/")))]
     (str in-path "/")))
 
+
 (defn samples [path config samples-filter]
-  (let [files (file-seq (file path))
+  (let [path (if (:src config)
+               (str path "/" (:src config))
+               path)
+        files (file-seq (file path))
         files* (filter #(and
                           (not (.isDirectory %))
                           (not (.isHidden %))
